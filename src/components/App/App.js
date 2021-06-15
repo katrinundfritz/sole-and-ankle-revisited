993ab6c582bcd5bf3a1a340adea251b8bbd6d72f
Katrin Fritz <9885028+katrinundfritz@users.noreply.github.com>
2021-06-15 09:18:47 +0200

    Setup breakpoints

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components/macro';
 
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
+import { QUERIES } from '../../constants'
 
 const App = () => {
   const [sortId, setSortId] = React.useState('newest');
@@ -19,6 +20,17 @@ const App = () => {
 
 const Main = styled.main`
   padding: 64px 32px;
+
+  /* @media ${QUERIES.laptopAndSmaller} {
+    background-color: red;
+
+  }
+  @media ${QUERIES.tabletAndSmaller} {
+    background-color: yellow;
+  }
+  @media ${QUERIES.phoneAndSmaller} {
+    background-color: green;
+  } */
 `;
 
 export default App;
