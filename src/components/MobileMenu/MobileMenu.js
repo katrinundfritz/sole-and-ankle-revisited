70ab60a939900211fa381e6b30fcb47a936ae1b9
Katrin Fritz <9885028+katrinundfritz@users.noreply.github.com>
2021-06-22 20:26:54 +0200

    Refactor mobile menu

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 
-import { COLORS } from '../../constants';
+import { COLORS, WEIGHTS } from '../../constants';
 
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
@@ -16,27 +16,25 @@ const MobileMenu = ({ isOpen, onDismiss }) => {
 
   return (
     <Overlay>
-      <Content aria-label=''>
-        <CloseButton>
-          <UnstyledButton onClick={onDismiss}>
-            <Icon id="close" strokeWidth={2} color={COLORS.gray[900]} />
-            <VisuallyHidden>Dismiss menu</VisuallyHidden>
-          </UnstyledButton>
+      <Content aria-label='Menu'>
+        <CloseButton onClick={onDismiss}>
+          <Icon id="close" strokeWidth={2} color={COLORS.gray[900]} />
+          <VisuallyHidden>Dismiss menu</VisuallyHidden>
         </CloseButton>
         <Filler />
         <Navigation>
-          <a href="/sale">Sale</a>
-          <a href="/new">New&nbsp;Releases</a>
-          <a href="/men">Men</a>
-          <a href="/women">Women</a>
-          <a href="/kids">Kids</a>
-          <a href="/collections">Collections</a>
+          <NavLink href="/sale">Sale</NavLink>
+          <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
+          <NavLink href="/women">Women</NavLink>
+          <NavLink href="/kids">Kids</NavLink>
+          <NavLink href="/collections">Collections</NavLink>
         </Navigation>
         <Filler>
           <Footer>
-            <a href="/terms">Terms and Conditions</a>
-            <a href="/privacy">Privacy Policy</a>
-            <a href="/contact">Contact Us</a>
+            <FooterLink href="/terms">Terms and Conditions</FooterLink>
+            <FooterLink href="/privacy">Privacy Policy</FooterLink>
+            <FooterLink href="/contact">Contact Us</FooterLink>
           </Footer>
         </Filler>
       </Content>
@@ -52,8 +50,7 @@ const Overlay = styled(DialogOverlay)`
   bottom: 0;
   background: hsl(0deg 0% 0% / 0.5);
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-end;
   `;
 
 const Content = styled(DialogContent)`
@@ -67,11 +64,12 @@ const Content = styled(DialogContent)`
   flex-direction: column;
 `;
 
-const CloseButton = styled.div`
+const CloseButton = styled(UnstyledButton)`
   position: absolute;
-  right: 22px;
-  top: 32px;
+  right: 0px;
+  top: 10px;
   cursor: pointer;
+  padding: 16px;
 `;
 
 const Filler = styled.div`
@@ -83,14 +81,17 @@ const Navigation = styled.nav`
   flex-direction: column;
   text-transform: uppercase;
   text-decoration: none;
-  gap: 22px;
+  gap: 16px;
+`
 
-  & a {
-    color: ${COLORS.gray[900]};
-    text-decoration: none;
-  }
+const NavLink = styled.a`
 
-  & a:first-of-type {
+  font-size: 1.125rem;
+  color: ${COLORS.gray[900]};
+  font-weight: ${WEIGHTS.medium};
+  text-decoration: none;
+
+  &:first-of-type {
     color: ${COLORS.secondary};
   }
 `
@@ -100,15 +101,14 @@ const Footer = styled.footer`
   display: flex;
   flex-direction: column;
   text-decoration: none;
-  gap: 14px;
+  gap: 8px;
   justify-content: flex-end;
+`
 
-  & a {
-    font-size: 0.875rem;
-    color: ${COLORS.gray[700]};
-    text-decoration: none;
-  }
-
+const FooterLink = styled.a`
+  font-size: 0.875rem;
+  color: ${COLORS.gray[700]};
+  text-decoration: none;
 `
 
 export default MobileMenu;
